1801d13e93e2018ace2daf017160e9383e8b9bec
enotMain <777_Gaga@mail.ru>
2020-04-08 20:18:01 +0300

    first commit

---
+++ src/main/java/domain/CompanyGeneral.java
@@ -0,0 +1,21 @@
+package domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class CompanyGeneral {
+    @Id
+    private Long Id;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+}
