455711fd3f474611b5ddbc1dc4e79bb6ec434e74
enotMain <777_Gaga@mail.ru>
2020-04-08 20:12:05 +0300

    Added "@Inheritance(strategy = JOINED)" and CompanyAbstract renamed to CompanyGeneral

--- src/main/java/domain/Company.java
+++ src/main/java/domain/Company.java
@@ -4,7 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
-public class Company extends CompanyAbstract {
+public class Company extends CompanyGeneral {
     @Column
     private String name;
     @Column
